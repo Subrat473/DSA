c9176c0809fe111f26d33beaeff0848b9a2743f2
Subrart437 <subratpathak95@gmail.com>
2025-07-22 23:09:47 +0530

    Basics D1

---
+++ Lecture_1_Master_the_Basics/Basic addition.js
@@ -0,0 +1,26 @@
+// Basic logic on JS -> Variable, operator, operation on operator
+
+
+
+
+// Sum of two numbers 
+
+
+// let a  = 10;
+// let b = 20;
+
+// console.log(a+b);
+
+
+
+// // sum of message 
+
+// console.log("Sum of 10 and 20 is "(a+b));
+
+
+
+
+let age = prompt("What is your age");
+
+console.log(age   );
+
